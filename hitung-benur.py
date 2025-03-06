bf97642d7bb5a591e509d58fd899eadb9767ae53
Riyantttsjddj <135335611+Riyantttsjddj@users.noreply.github.com>
2025-03-06 15:03:48 +0800

    hitung-benur.py

--- hitung-benur.py
+++ hitung-benur.py
@@ -28,7 +28,7 @@ while True:
     cv2.putText(frame, f"Benur: {jumlah_benur}", (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
     # Tampilkan frame
-    cv2.imshow("Deteksi Benur", frame)
+    cv2.imwrite("hasil.jpg", frame)
 
     # Tekan 'q' untuk keluar
     if cv2.waitKey(1) & 0xFF == ord('q'):
